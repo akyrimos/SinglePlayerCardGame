993bff979ebea641de0bf0d3961fe97469ecb921
akyrimos <maasongmx@gmail.com>
2022-04-19 17:29:47 -0700

    Revert "Revert "Update card header in Card.h""
    This reverts commit 3e53bf604bcdcd327a9e1813192c31f8f3ee3b40.

--- Include/Card.h
+++ Include/Card.h
@@ -5,6 +5,27 @@
 #include <time.h>
 #include <vector>
 #include "VecMat.h"
+#include "Sprite.h"
+#include "CardEnum.h"
 
+
+using std::string;
+
+
+class Card : public Sprite {
+
+public:
+	int energyCost;
+	string cardName;
+
+	struct abilities {
+		CardEnum h;
+		int value; 
+};
+
+private:
+	int ID;
+
+};
 
 #endif
