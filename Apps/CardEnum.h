ad4d3587bea22ecc156ff15cdcaac2151ef9d063
akyrimos <maasongmx@gmail.com>
2022-04-19 17:14:54 -0700

    update enum name to cardAbility in CardEnum.h

--- Apps/CardEnum.h
+++ Apps/CardEnum.h
@@ -2,5 +2,5 @@
 using namespace std;
 
 class CardEnum {
-	enum card { Attack = 5, Defend = 10, Debuff = 15, Buff = 20, Power = 25, Undefined =0,};
+	enum cardAbility { Attack = 5, Defend = 10, Debuff = 15, Buff = 20, Power = 25, Undefined =0,};
 };
