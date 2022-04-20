3e53bf604bcdcd327a9e1813192c31f8f3ee3b40
akyrimos <maasongmx@gmail.com>
2022-04-19 17:29:20 -0700

    Revert "Update card header in Card.h"
    This reverts commit 7f2a368deece86efe7d41d0f8dd41bec813b5fbc.

--- Apps/CardEnum.h
+++ Apps/CardEnum.h
@@ -3,12 +3,4 @@ using namespace std;
 
 class CardEnum {
 	enum cardAbility { Attack = 5, Defend = 10, Debuff = 15, Buff = 20, Power = 25, Undefined =0,};
-
-	/**
-	* TODO: set up switch case for getAbility
-	* TODO: create setAbility, maybe in Card class? 
-	**/
-	CardEnum::cardAbility getAbility(CardEnum::cardAbility a) {
-		return Attack;
-	}
 };
