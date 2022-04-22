5151eff55ed9c831912ce62b6d1e21d11b0c172e
akyrimos <maasongmx@gmail.com>
2022-04-21 17:35:07 -0700

    Add getter and setter to CardEnum.h

--- Apps/CardEnum.h
+++ Apps/CardEnum.h
@@ -2,7 +2,17 @@
 using namespace std;
 
 class CardEnum {
-	enum cardAbility { Attack = 5, Defend = 10, Debuff = 15, Buff = 20, Power = 25, Undefined =0,};
+	enum cardAbility { Attack = 5, Defend = 10, Debuff = 15, Buff = 20, Power = 25, Undefined = 0,};
+
+	cardAbility ability;
+
+	cardAbility getAbility() {
+		return ability;
+	}
+
+	void setAbility(cardAbility newAbility) {
+		ability = newAbility;
+	}
 
 	
 
