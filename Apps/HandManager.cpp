a5895d6b77dc7756c7ea3c3fb5f31f8e07859126
Shawn Lee <slee4@seattleu.edu>
2022-05-03 17:37:23 -0700

    Added HandManager.h and .cpp Constructor, destructor, nad Draw()

--- Apps/HandManager.cpp
+++ Apps/HandManager.cpp
@@ -1,7 +1,25 @@
 #include "HandManager.h"
 
 HandManager::HandManager() {
-	cardPtr = new Card[handSize];
-
+	vector<Card> hand;
+	libptr = new Library();
 }
+
+HandManager::~HandManager()
+{
+	delete &hand;
+	// not sure if HandManager is responsible for Library
+	// delete library;
+}
+
+void HandManager::Draw() {
+	if (handSize >= maxHandSize) return;
+	hand.push_back(libptr->Draw());
+	handSize++;
+}
+
+
+
+
+
 
--- Include/HandManager.h
+++ Include/HandManager.h
@@ -2,10 +2,24 @@
 #define HANDMANAGER
 #pragma once
 #include "Card.h"
+#include "Library.h"
+#include "vector"
+
 class HandManager
 {
-	int handSize = 10;
-	Card* cardPtr;
+public:
 	HandManager();
+	~HandManager();
+	void playCard(Card c);
+private:
+	int maxHandSize = 10;
+	int drawCards = 5;
+	int handSize = 0;
+	void Draw();
+	void Discard();
+	vector<Card> hand;
+	Library* libptr;
+
+
 };
 #endif
