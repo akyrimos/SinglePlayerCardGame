693070d98ed2c95eaeec2ab4e374c0ccc06d0f41
akyrimos <maasongmx@gmail.com>
2022-05-05 16:27:12 -0700

    Updating constructor and RunTurn() in TurnManager.h and TurnManager.cpp

--- Apps/TurnManager.cpp
+++ Apps/TurnManager.cpp
@@ -1,1 +1,22 @@
 #include "TurnManager.h"
+
+using namespace std;
+
+TurnManager::TurnManager(Library lib, HandManager hand) {
+
+	this->libptr = &lib;
+	this->handptr = &hand;
+
+}
+
+TurnManager::~TurnManager() {
+
+}
+
+void TurnManager::RunTurn() {
+
+	for (int i = 0; i < drawForTurn; i++) {
+		handptr->Draw();
+	}
+
+}
--- Apps/TurnManager.h
+++ Apps/TurnManager.h
@@ -1,14 +1,27 @@
+#include "Enemy.h"
+#include "../Include/Actor.h"
+#include "Library.h"
+#include "../Include/HandManager.h"
+
 #pragma once
+
 class TurnManager
 {
 public:
 
 	int turnNum = 1;
+	int drawForTurn = 5;
 
-	TurnManager();
+	TurnManager(Library libptr, HandManager handptr);
 	~TurnManager();
+
+	void RunTurn();
 	
 
 private:
+
+	Library* libptr;
+	HandManager* handptr;
+
 };
 
